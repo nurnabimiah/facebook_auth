c72b31de8d2d4a61c337fa011661c2c65954ae8b
Nurnabi <diunayonahmed@gmail.com>
2023-02-18 13:39:59 +0600

    First commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.blue, accentColor: Colors.yellow),
+      theme: ThemeData(primarySwatch: Colors.blue, accentColor: Colors.black),
       home: const FacebookAuth(),
     );
   }
